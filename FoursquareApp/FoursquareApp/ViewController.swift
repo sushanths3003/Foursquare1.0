04360fcf4af62f5bd3eefb6fddc71f446254810d
sushanth <sushanth.s@robosoftin.com>
2021-06-29 11:24:03 +0530

    first commit

---
+++ FoursquareApp/FoursquareApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoursquareApp
+//
+//  Created by Sushanth S on 29/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
