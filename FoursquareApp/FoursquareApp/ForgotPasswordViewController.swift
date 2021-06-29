8bb3d9e1bb5c0315243f7d8979f069225e93fe47
Bhoomika <bhoomika.hp@robosoftin.com>
2021-06-29 14:43:15 +0530

    login and Forgot password VC

---
+++ FoursquareApp/FoursquareApp/ForgotPasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ForgotPasswordViewController.swift
+//  FoursquareApp
+//
+//  Created by Bhoomika H P on 29/06/21.
+//
+
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.isHidden = true
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
